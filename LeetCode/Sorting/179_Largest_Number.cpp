12f39afe545a581e557c216f38047387d50f533e
UrCoderZayan <zayankamran24@gmail.com>
2026-08-11 16:00:42 +0500

    Solved Leetcode 179 Largest Number

---
+++ LeetCode/Sorting/179_Largest_Number.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+
+        vector<string> arr;
+
+        for (int num : nums) {
+            arr.push_back(to_string(num));
+        }
+
+        sort(arr.begin(), arr.end(),
+             [](const string& a, const string& b) {
+                 return a + b > b + a;
+             });
+
+        if (arr[0] == "0") {
+            return "0";
+        }
+
+        string result;
+
+        for (const string& num : arr) {
+            result += num;
+        }
+
+        return result;
+    }
+};
--- READme.md
+++ READme.md
@@ -53,3 +53,5 @@ This repository documents my 42-day journey of learning Data Structures and Algo
 | 1051 | Height Checker | Easy | O(n log n) | O(n) | ✅ |
 | 56 | Merge Intervals | Medium | O(n log n) | O(n) | ✅ |
 | 435 | Non-overlapping Intervals | Medium | O(n log n) | O(1) | ✅ |
+| 347 | Top K Frequent Elements | Medium | O(n log n) | O(n) | ✅ |
+| 179 | Largest Number | Medium | O(n log n) | O(n) | ✅ |
