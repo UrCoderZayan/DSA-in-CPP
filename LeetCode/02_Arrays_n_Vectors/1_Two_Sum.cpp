b46a7085b02768b6a6d4fb69ab0b73f42e0fc8f9
UrCoderZayan <zayankamran24@gmail.com>
2026-08-30 22:27:58 +0500

    Solve LeetCode 1 - Two Sum

---
+++ LeetCode/02_Arrays_n_Vectors/1_Two_Sum.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indexMap;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (indexMap.find(complement) != indexMap.end()) {
+                return {indexMap[complement], i};
+            }
+
+            indexMap[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
